aca2791e2000f9200ab0047be8baa321c54ffa19
Sergey Savelyev <sergeysav.nn@gmail.com>
2018-09-19 22:09:08 -0400

    Adds import

--- app/src/main/java/edu/gatech/orangeblasters/LoginActivity.java
+++ app/src/main/java/edu/gatech/orangeblasters/LoginActivity.java
@@ -2,6 +2,7 @@ package edu.gatech.orangeblasters;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
