b8f59ae8a52483dcf05929274cd793d5ab282c70
rcefalu10 <rcefalu10@gmail.com>
2018-10-01 21:17:46 -0400

    Added a user type enum

---
+++ app/src/main/java/edu/gatech/orangeblasters/Account/AccountTypes.java
@@ -0,0 +1,35 @@
+package edu.gatech.orangeblasters.Account;
+
+public enum AccountTypes {
+    ADMIN("Admin"),
+    MANAGER("Manager"),
+    EMPLOYEE("Location Employee"),
+    USER("User"),;
+
+    /**
+     * 2 letter abbreviation of class standing
+     */
+    private String userType;
+
+    /**
+     * Constructor for the enumeration
+     *
+     * @param userType the user type
+     */
+    AccountTypes(String userType) {
+        this.userType = userType;
+    }
+
+    /**
+     * @return the user type
+     */
+    public String getUserType() {
+        return userType;
+    }
+    /**
+     * @return the user type
+     */
+    public String setUserType(String userType) {
+        return this.userType = userType;
+    }
+}
