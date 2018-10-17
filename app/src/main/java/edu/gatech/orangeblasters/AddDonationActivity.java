1b971bf60a3d7f394d61b71d5e60812c2cbaa698
Sandy <sxie@gatech.edu>
2018-10-17 15:20:21 -0400

    added screen for uploading donation item

--- app/src/main/java/edu/gatech/orangeblasters/AddDonationActivity.java
+++ app/src/main/java/edu/gatech/orangeblasters/AddDonationActivity.java
@@ -2,12 +2,32 @@ package edu.gatech.orangeblasters;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Button;
 
-public class AddDonationActivity extends AppCompatActivity {
+public class AddDonationActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView imageUpload;
+    Button uploadButton;
+    EditText donationComments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_donation);
+
+        imageUpload = (ImageView) findViewById(R.id.imageUpload);
+        uploadButton = (Button) findViewById(R.id.uploadButton);
+        donationComments = (EditText) findViewById(R.id.donationComments);
+
+        imageUpload.setOnClickListener(this);
+        uploadButton.setOnClickListener(this);
+        donationComments.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
