96d1935d3f2746f7457b30353ad2c143660b645a
Sergey Savelyev <sergeysav.nn@gmail.com>
2018-10-31 14:36:31 -0400

    adds search for locations

--- app/src/main/java/edu/gatech/orangeblasters/LocationListActivity.java
+++ app/src/main/java/edu/gatech/orangeblasters/LocationListActivity.java
@@ -4,15 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.recyclerview.extensions.ListAdapter;
-import android.support.v7.util.DiffUtil;
+import android.support.v7.util.SortedList;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
+
+import java.util.function.Function;
 
 import edu.gatech.orangeblasters.location.Location;
 
@@ -20,6 +22,8 @@ public class LocationListActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLayoutManager;
+
+    private Function<Location, Integer> relevanceFilter = __ -> 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +42,56 @@ public class LocationListActivity extends AppCompatActivity {
                 mLayoutManager.getOrientation());
         mRecyclerView.addItemDecoration(dividerItemDecoration);
 
-        adapter.submitList(OrangeBlastersApplication.getInstance().getLocationService().getLiveIDList().getValue());
-        OrangeBlastersApplication.getInstance().getLocationService().getLiveIDList().observe(this, adapter::submitList);
+        doUpdateFilteredList(adapter);
+
+        OrangeBlastersApplication.getInstance().getLocationService().getLiveIDList().observe(this, (list) -> {
+            //When the id list changes
+            doUpdateFilteredList(adapter);
+        });
+
         mRecyclerView.setAdapter(adapter);
+
+        SearchView searchBar = findViewById(R.id.searchbar);
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return onQueryTextChange(query);
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                relevanceFilter = location -> {
+                    if (newText.isEmpty()) {
+                        return 1;
+                    }
+
+                    String text = newText.toLowerCase();
+                    return (location.getName().equalsIgnoreCase(text) ? 20 : 0) //Exact name = 20 points
+                            + (location.getName().toLowerCase().contains(text) ? 5 : 0) //Contains name = 5 point
+                            + (location.getType().getFullName().toLowerCase().contains(text) ? 2 : 0) //Type = 2 point
+                            + (location.getAddress().toLowerCase().contains(text) ? 2 : 0) //Address = 2 point
+                            + ((int)location.getDonations().stream().filter(donation ->  //1 point per relevant donation
+                            donation.getDescShort().toLowerCase().contains(text) ||
+                                    donation.getDescShort().toLowerCase().contains(text) ||
+                                    donation.getComments().map(str -> str.toLowerCase().contains(text)).orElse(false)).count());
+                };
+                doUpdateFilteredList(adapter);
+                return true;
+            }
+        });
     }
 
-    public static class LocationAdapter extends ListAdapter<String, LocationAdapter.LocationViewHolder> {
+    private void doUpdateFilteredList(LocationAdapter adapter) {
+        adapter.getSortedList().beginBatchedUpdates();
+        adapter.getSortedList().clear();
+        OrangeBlastersApplication.getInstance().getLocationService().getLocations()
+                //relevance has to be greater than 0
+                .filter(loc -> relevanceFilter.apply(loc) > 0)
+                .forEach(adapter.getSortedList()::add);
+        adapter.getSortedList().endBatchedUpdates();
+    }
 
-        public LocationAdapter() {
-            super(DIFF_CALLBACK);
-        }
+    public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.LocationViewHolder> {
 
         @NonNull
         @Override
@@ -60,25 +104,62 @@ public class LocationListActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull LocationViewHolder holder, int position) {
-            holder.bind(OrangeBlastersApplication.getInstance().getLocationService().getLocation(getItem(position)).orElse(null));
+            holder.bind(sortedList.get(position));
 
         }
 
-        public static final DiffUtil.ItemCallback<String> DIFF_CALLBACK =
-                new DiffUtil.ItemCallback<String>() {
-                    @Override
-                    public boolean areItemsTheSame(
-                            @NonNull String oldUser, @NonNull String newUser) {
-                        return oldUser.equals(newUser);
-                    }
-                    @Override
-                    public boolean areContentsTheSame(
-                            @NonNull String oldUser, @NonNull String newUser) {
-                        return oldUser.equals(newUser);
-                    }
-                };
+        @Override
+        public int getItemCount() {
+            return sortedList.size();
+        }
 
-        public static class LocationViewHolder extends RecyclerView.ViewHolder {
+        private final SortedList<Location> sortedList = new SortedList<>(Location.class, new SortedList.Callback<Location>() {
+            @Override
+            public int compare(Location o1, Location o2) {
+                Integer relevence1 = relevanceFilter.apply(o1);
+                Integer relevence2 = relevanceFilter.apply(o2);
+                //Compare by relevance then by caseless name
+                if (relevence1.compareTo(relevence2) != 0) {
+                    return relevence1.compareTo(relevence2);
+                }
+                return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+
+            @Override
+            public void onChanged(int position, int count) {
+                notifyItemRangeChanged(position, count);
+            }
+
+            @Override
+            public boolean areContentsTheSame(Location oldItem, Location newItem) {
+                return oldItem.equals(newItem);
+            }
+
+            @Override
+            public boolean areItemsTheSame(Location item1, Location item2) {
+                return item1.getId().equals(item2.getId());
+            }
+
+            @Override
+            public void onInserted(int position, int count) {
+                notifyItemRangeInserted(position, count);
+            }
+
+            @Override
+            public void onRemoved(int position, int count) {
+                notifyItemRangeRemoved(position, count);
+            }
+
+            @Override
+            public void onMoved(int fromPosition, int toPosition) {
+                notifyItemMoved(fromPosition, toPosition);
+            }
+        });
+
+        public SortedList<Location> getSortedList() {
+            return sortedList;
+        }
+        public class LocationViewHolder extends RecyclerView.ViewHolder {
             private final TextView textView;
             private Location location;
 
