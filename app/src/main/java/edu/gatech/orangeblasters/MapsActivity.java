edf47da3a5887ba574c216e33f0fade86037fbf6
rcefalu10 <rcefalu10@gmail.com>
2018-11-06 15:03:57 -0500

    fixing riley's errors

--- app/src/main/java/edu/gatech/orangeblasters/MapsActivity.java
+++ app/src/main/java/edu/gatech/orangeblasters/MapsActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.Optional;
 
 import edu.gatech.orangeblasters.location.Location;
+import edu.gatech.orangeblasters.location.LocationService;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
